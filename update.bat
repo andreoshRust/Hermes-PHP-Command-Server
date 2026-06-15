f619c1de24330ee1641c3806ed58d64baaa6398a
unknown <andreoshmi@gmail.com>
2026-06-15 09:59:51 +0300

    Update from 15.06.2026 9:59:51

---
+++ update.bat
@@ -0,0 +1,24 @@
+@echo off
+cd /d C:\andreosh\GitHab_my\Hermes-PHP-Command-Server
+
+echo ========================================
+echo    Hermes-PHP-Command-Server Repository Update
+echo ========================================
+echo.
+
+git add .
+
+echo Files to be committed:
+git status --short
+echo.
+
+for /f "tokens=1-3 delims=.: " %%a in ('echo %time%') do set "mytime=%%a:%%b:%%c"
+git commit -m "Update from %date% %mytime%"
+
+git push origin master
+
+echo.
+echo ========================================
+echo    Done!
+echo ========================================
+pause
